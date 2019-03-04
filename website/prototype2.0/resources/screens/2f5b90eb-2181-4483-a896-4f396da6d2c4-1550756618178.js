38d5feb422ccce1558f8c5b5438791b68f5daa1d
zhao-yue <610714452@Qq.com>
2019-03-04 10:35:17 +0000

    First commit - front end codes

---
+++ website/prototype2.0/resources/screens/2f5b90eb-2181-4483-a896-4f396da6d2c4-1550756618178.js
@@ -0,0 +1,456 @@
+jQuery("#simulation")
+  .on("click", ".s-2f5b90eb-2181-4483-a896-4f396da6d2c4 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Input_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow_1" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-arrow_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow_1" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Input_5")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow_2" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-arrow_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow_2" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#s-2f5b90eb-2181-4483-a896-4f396da6d2c4 #s-Rectangle_4 > .backgroundLayer": {
+                      "attributes": {
+                        "background-color": "#000000",
+                        "background-attachment": "scroll"
+                      }
+                    }
+                  },{
+                    "#s-2f5b90eb-2181-4483-a896-4f396da6d2c4 #s-Rectangle_4": {
+                      "attributes-ie": {
+                        "-pie-background": "#000000",
+                        "-pie-poll": "false"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Input_8")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow_1" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Input_9")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow_4" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-arrow_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimRotate",
+                  "parameter": {
+                    "target": [ "#s-arrow_4" ],
+                    "angle": {
+                      "type": "rotateby",
+                      "value": "180"
+                    },
+                    "effect": {
+                      "type": "none",
+                      "easing": "swing",
+                      "duration": 400
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("drag", ".s-2f5b90eb-2181-4483-a896-4f396da6d2c4 .drag", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_6")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": [ "#s-Rectangle_6" ],
+                    "top": {
+                      "type": "nomove"
+                    },
+                    "left": {
+                      "type": "movewithcursor",
+                      "value": null
+                    },
+                    "containment": true
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_3")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": [ "#s-Rectangle_3" ],
+                    "top": {
+                      "type": "nomove"
+                    },
+                    "left": {
+                      "type": "movewithcursor",
+                      "value": null
+                    },
+                    "containment": true
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_8")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": [ "#s-Rectangle_8" ],
+                    "top": {
+                      "type": "nomove"
+                    },
+                    "left": {
+                      "type": "movewithcursor",
+                      "value": null
+                    },
+                    "containment": true
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Rectangle_10")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": [ "#s-Rectangle_10" ],
+                    "top": {
+                      "type": "nomove"
+                    },
+                    "left": {
+                      "type": "movewithcursor",
+                      "value": null
+                    },
+                    "containment": true
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("dragend", ".s-2f5b90eb-2181-4483-a896-4f396da6d2c4 .drag", function(event, data) {
+    jimEvent(event).jimRestoreDrag(jQuery(this));
+  })
+  .on("dragend", ".s-2f5b90eb-2181-4483-a896-4f396da6d2c4 .drag", function(event, data) {
+    jimEvent(event).jimDestroyDrag(jQuery(this));
+  })
+  .on("mouseenter dragenter", ".s-2f5b90eb-2181-4483-a896-4f396da6d2c4 .mouseenter", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_4") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#s-2f5b90eb-2181-4483-a896-4f396da6d2c4 #s-Rectangle_4 > .backgroundLayer": {
+                      "attributes": {
+                        "background-color": "#999999",
+                        "background-attachment": "scroll"
+                      }
+                    }
+                  },{
+                    "#s-2f5b90eb-2181-4483-a896-4f396da6d2c4 #s-Rectangle_4": {
+                      "attributes-ie": {
+                        "-pie-background": "#999999",
+                        "-pie-poll": "false"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseleave dragleave", ".s-2f5b90eb-2181-4483-a896-4f396da6d2c4 .mouseleave", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_4")) {
+      jEvent.undoCases(jFirer);
+    }
+  });
---
+++ website/prototype2.0/resources/simulationdata-1550756618178.js
@@ -0,0 +1,270 @@
+function initData() {
+  jimData.datamasters["data"] = [
+    {
+      "id": 1,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": "Rank,Name,Graduation rates,Employment chances,Quality of life,Teaching excellence",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 2,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": "1,Massachusetts Institute of Technology (MIT),91.3%,75.1%,77,85",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 3,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": "2,Stanford University,93%,79%,75,86",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 4,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": "3,Harvard University,92.2%,72%,76,93",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 5,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": "4,California Institute of Technology (Caltech),89%,81%,70,89",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 6,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": "5,University of Oxford,86%,75.8%,80,76",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 7,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": "6,University of Cambridge,84.3%,80%,81,75",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 8,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": "7,ETH Zurich - Swiss Federal Institute of Technology,86%,82%,69,72",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 9,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": "8,Imperial College London,84.4%,82.3%,67,73",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 10,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": "9,University of Chicago,87.7%,73%,65,67",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 11,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": "10,UCL (University College London),82.2%,76%,75,62",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 12,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": ",,,,,",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 13,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": ",,,,,",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 14,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": ",,,,,",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 15,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": ",,,,,",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 16,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": ",,,,,",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 17,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": ",,,,,",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 18,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": ",,,,,",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 19,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": ",,,,,",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 20,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": ",,,,,",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 21,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": ",,,,,",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    },
+    {
+      "id": 22,
+      "datamaster": "data",
+      "userdata": {
+        "Rank": ",,,,,",
+        "Name": "",
+        "Graduation rates": "",
+        "Employment chances": "",
+        "Quality of life": "",
+        "Teaching excellence": ""
+      }
+    }
+  ];
+
+  jimData.isInitialized = true;
+}
