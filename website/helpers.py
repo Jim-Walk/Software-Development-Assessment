e640aaaff10df746e4f6dfc0461b64e72977682a
andrewpsuedonym <j.m.walker@live.co.uk>
2019-03-04 17:14:12 +0000

    page rank and front page

--- website/helpers.py
+++ website/helpers.py
@@ -8,8 +8,7 @@ def get_logo(uni_title):
     url ='https://www.googleapis.com/customsearch/v1?searchType=image&cx=011314089285855499136%3Ata88ozaawbs&num=1&fields=items(link)'+config.API_KEY
     # construct query string using a list comprhension
     # convert spaces to %20
-    query = '&q=' + ''.join(['%20' if char is ' ' else char for char in
-                             uni_title]) + '%20logo'
+    query = '&q=' + uni_title.replace(' ', '%20') + '%20logo'
     r = requests.get(url+query)
     return r.json()['items'][0]['link']
 
